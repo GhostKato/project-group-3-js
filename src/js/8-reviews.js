ed2ef5a1bf8e753f2b285e48b1f2e9b6c8c387bd
nataliakostyrko <155002144+nataliakostyrko@users.noreply.github.com>
2024-06-15 16:28:56 +0200

    commit

---
+++ src/js/8-reviews.js
@@ -0,0 +1,45 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { doFetch } from './8-reviews2';
+
+const SCREEN_WIDTH = window.innerWidth;
+let slidesPerView;
+
+if (SCREEN_WIDTH >= 1280) {
+  slidesPerView = 4;
+} else if (SCREEN_WIDTH >= 768 && SCREEN_WIDTH < 1280) {
+  slidesPerView = 2;
+} else if (SCREEN_WIDTH < 768) {
+  slidesPerView = 1;
+}
+
+function createSwiper() {
+  const SWIPER = new Swiper('.reviews-swiper', {
+    cssMode: true,
+    slidesPerView: slidesPerView,
+    slidesPerGroup: 1,
+    navigation: {
+      disabledClass: "BtnOff",
+      nextEl: '.ButtonNext',
+      prevEl: '.ButtonPrev',
+    },
+    keyboard: true,
+    touch: true,
+  });
+}
+
+function doMurkUp(review) {
+    return `
+  <li class="reviews-list-item swiper-slide">
+  <p class="ReviewText">${review.review}</p>
+    <div class="Review swiper-slide">
+        <img class="UserIcon" src="${review.avatar_url}" alt="Avatar"  
+        width="32"
+        height="32"  >
+          <h3 class="Name">${review.author}</h3>
+          
+        </div>
+        </li>`;
+}
+
+doFetch(createSwiper, doMurkUp);
---
+++ src/js/8-reviews2.js
@@ -0,0 +1,49 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+
+export async function PostRequest(email, comment) {
+  const BASE_URL = 'https://portfolio-js.b.goit.study/api';
+  const END_POINT = '/reviews';
+  const url = BASE_URL + END_POINT;
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({ email, comment }),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  };
+
+  return await fetch(url, options).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.status);
+    }
+    return resp.json();
+  });
+}
+
+export function doFetch(foo, murkUpFoo) {
+  const REVIEWS_LIST = document.getElementById('reviewsList');
+  fetch("https://portfolio-js.b.goit.study/api/reviews")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(reviews => {
+      const markup = reviews.map(review => murkUpFoo(review)).join("");
+      REVIEWS_LIST.innerHTML = markup;
+      foo();
+    })
+    .catch(error => {
+      console.error(error);
+      iziToast.error({
+        title: 'Помилка',
+        message: 'Не вдалося завантажити список відгуків',
+      });
+      REVIEWS_LIST.innerHTML = "Not found";
+    });
+}
