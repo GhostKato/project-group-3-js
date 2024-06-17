7ec12c17f6018494bdba153a43da1c68d63faa8d
GhostKato <osadchenko.maksym@gmail.com>
2024-06-17 15:13:06 +0300

    fixed-night-mode

--- src/js/night-mode.js
+++ src/js/night-mode.js
@@ -1,18 +1,36 @@
 function autoNightMode() {
     const currentHour = new Date().getHours();
     const toggleSwitch = document.getElementById('toggle-night-mode');
+    const nightMode = localStorage.getItem('nightMode');
 
-    if (currentHour >= 18 || currentHour < 6) {
-        document.body.classList.add('night-mode');
-        toggleSwitch.checked = true;
+    if (nightMode) {
+        if (nightMode === 'enabled') {
+            document.body.classList.add('night-mode');
+            toggleSwitch.checked = true;
+        } else {
+            document.body.classList.remove('night-mode');
+            toggleSwitch.checked = false;
+        }
     } else {
-        document.body.classList.remove('night-mode');
-        toggleSwitch.checked = false;
+        if (currentHour >= 18 || currentHour < 6) {
+            document.body.classList.add('night-mode');
+            toggleSwitch.checked = true;
+            localStorage.setItem('nightMode', 'enabled');
+        } else {
+            document.body.classList.remove('night-mode');
+            toggleSwitch.checked = false;
+            localStorage.setItem('nightMode', 'disabled');
+        }
     }
 }
 
 document.getElementById('toggle-night-mode').addEventListener('change', function () {
     document.body.classList.toggle('night-mode');
+    if (document.body.classList.contains('night-mode')) {
+        localStorage.setItem('nightMode', 'enabled');
+    } else {
+        localStorage.setItem('nightMode', 'disabled');
+    }
 });
 
 window.onload = autoNightMode;
