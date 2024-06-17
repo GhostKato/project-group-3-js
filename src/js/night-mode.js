d2dbd6467bed230d88095c4873b88cae9b572d30
GhostKato <osadchenko.maksym@gmail.com>
2024-06-17 15:57:08 +0300

    fixed

--- src/js/night-mode.js
+++ src/js/night-mode.js
@@ -1,26 +1,33 @@
- document.addEventListener('DOMContentLoaded', function() {
+ import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.addEventListener('DOMContentLoaded', function () {
             function autoNightMode() {
                 const currentHour = new Date().getHours();
                 const toggleSwitch = document.getElementById('toggle-night-mode');
-                const nightMode = localStorage.getItem('nightMode');
+                const nightMode = localStorage.getItem('nightMode');                
 
                 if (nightMode) {
                     if (nightMode === 'enabled') {
                         document.body.classList.add('night-mode');
-                        toggleSwitch.checked = true;
+                        toggleSwitch.checked = true;                      
+                        
                     } else {
                         document.body.classList.remove('night-mode');
-                        toggleSwitch.checked = false;
+                        toggleSwitch.checked = false;                        
+                        
                     }
                 } else {
                     if (currentHour >= 18 || currentHour < 6) {
                         document.body.classList.add('night-mode');
                         toggleSwitch.checked = true;
-                        localStorage.setItem('nightMode', 'enabled');
+                        localStorage.setItem('nightMode', 'enabled');                        
+                        iziToast.success({ title: 'Night Mode', message: 'Enabled by time' });
                     } else {
                         document.body.classList.remove('night-mode');
                         toggleSwitch.checked = false;
-                        localStorage.setItem('nightMode', 'disabled');
+                        localStorage.setItem('nightMode', 'disabled');                        
+                        iziToast.error({ title: 'Night Mode', message: 'Disabled by time' });
                     }
                 }
             }
@@ -28,9 +35,12 @@
             document.getElementById('toggle-night-mode').addEventListener('change', function () {
                 document.body.classList.toggle('night-mode');
                 if (document.body.classList.contains('night-mode')) {
-                    localStorage.setItem('nightMode', 'enabled');
+                    localStorage.setItem('nightMode', 'enabled');                    
+                    iziToast.success({ title: 'Night Mode', message: 'Enabled' });
                 } else {
-                    localStorage.setItem('nightMode', 'disabled');
+                    localStorage.setItem('nightMode', 'disabled');                    
+                    iziToast.error({ title: 'Night Mode', message: 'Disabled' });
+                    
                 }
             });
 
